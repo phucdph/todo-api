6a46290f848f39c79e2ef91ab5400d657a2daf00
Phuc Dang <xxzz2807@gmail.com>
2018-07-19 10:30:14 +0700

    first commit

---
+++ api/models/Todo.js
@@ -0,0 +1,22 @@
+/**
+ * Todo.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+    completed: {
+      type: 'boolean', 
+      defaultsTo: false
+    }
+  },
+
+};
+
